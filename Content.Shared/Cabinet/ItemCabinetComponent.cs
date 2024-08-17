f59ca2537ebd995d93af2ec0dec39809b2218f8b
Pe4henika <pecenkasokoladnyj38@gmail.com>
2024-08-17 12:01:58 +0300

    Фиксы переезда

--- Content.Shared/Cabinet/ItemCabinetComponent.cs
+++ Content.Shared/Cabinet/ItemCabinetComponent.cs
@@ -1,25 +1,43 @@
+using Content.Shared.Containers.ItemSlots;
+using Robust.Shared.Audio;
 using Robust.Shared.GameStates;
-using Robust.Shared.Serialization;
 
 namespace Content.Shared.Cabinet;
 
 /// <summary>
-/// Used for entities that can be opened, closed, and can hold one item. E.g., fire extinguisher cabinets.
-/// Requires <c>OpenableComponent</c>.
+///     Used for entities that can be opened, closed, and can hold one item. E.g., fire extinguisher cabinets.
 /// </summary>
-[RegisterComponent, NetworkedComponent, Access(typeof(ItemCabinetSystem))]
+[RegisterComponent, NetworkedComponent, AutoGenerateComponentState(true)]
 public sealed partial class ItemCabinetComponent : Component
 {
     /// <summary>
-    /// Name of the <see cref="ItemSlot"/> that stores the actual item.
+    ///     Sound to be played when the cabinet door is opened.
     /// </summary>
-    [DataField]
-    public string Slot = "ItemCabinet";
-}
+    [DataField, AutoNetworkedField, ViewVariables(VVAccess.ReadWrite)]
+    public SoundSpecifier? DoorSound;
 
-[Serializable, NetSerializable]
-public enum ItemCabinetVisuals : byte
-{
-    ContainsItem,
-    Layer
+    /// <summary>
+    ///     The <see cref="ItemSlot"/> that stores the actual item. The entity whitelist, sounds, and other
+    ///     behaviours are specified by this <see cref="ItemSlot"/> definition.
+    /// </summary>
+    [DataField, ViewVariables]
+    public ItemSlot CabinetSlot = new();
+
+    /// <summary>
+    ///     Whether the cabinet is currently open or not.
+    /// </summary>
+    [DataField, AutoNetworkedField]
+    public bool Opened;
+
+    /// <summary>
+    /// The state for when the cabinet is open
+    /// </summary>
+    [DataField, AutoNetworkedField, ViewVariables(VVAccess.ReadWrite)]
+    public string? OpenState;
+
+    /// <summary>
+    /// The state for when the cabinet is closed
+    /// </summary>
+    [DataField, AutoNetworkedField, ViewVariables(VVAccess.ReadWrite)]
+    public string? ClosedState;
 }
