f3836dfc50b373af4cc28f4aeecaddebc60e340e
TAZIKLIK <73418250+TAZIKLIK@users.noreply.github.com>
2024-08-19 20:49:01 +0300

    фиксы
    (cherry picked from commit 8a35314b673b301dcfa4cd1a3fd5f59f62b3b627)

--- Content.Client/Language/LanguageMenuWindow.xaml.cs
+++ Content.Client/Language/LanguageMenuWindow.xaml.cs
@@ -71,7 +71,7 @@ public sealed partial class LanguageMenuWindow : DefaultWindow
             HorizontalExpand = true
         };
 
-        var button = new Button { Text = "Choose" };
+        var button = new Button { Text = "Выбрать" };
         button.OnPressed += _ => OnLanguageChosen(language);
         state.button = button;
 
