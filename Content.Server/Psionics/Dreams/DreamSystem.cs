2af59edfa21705b2407e66d6d0c948b1ffd1c447
Den Frae <denfrae54@gmail.com>
2024-10-17 10:50:30 +0300

    Система сновидений

--- Content.Server/Psionics/Dreams/DreamSystem.cs
+++ Content.Server/Psionics/Dreams/DreamSystem.cs
@@ -1,57 +1,109 @@
 using Content.Shared.Dataset;
 using Content.Shared.Bed.Sleep;
-using Content.Server.Chat.Systems;
 using Content.Server.Chat.Managers;
 using Robust.Shared.Random;
 using Robust.Shared.Prototypes;
-using Robust.Server.GameObjects;
 using Robust.Shared.Player;
+using System.Threading.Tasks;
+using Content.Server.Mood;
 
 namespace Content.Server.Psionics.Dreams
 {
-    public sealed class DreamsSystem : EntitySystem
+    public sealed class DreamsSystem : EntitySystem // Redacted by Silly Elf Farrellka~ <3
     {
-        [Dependency] private readonly ChatSystem _chat = default!;
         [Dependency] private readonly IRobustRandom _random = default!;
         [Dependency] private readonly IChatManager _chatManager = default!;
         [Dependency] private readonly IPrototypeManager _prototypeManager = default!;
+
         private float _accumulator = 0f;
         private float _updateRate = 15f;
 
         public readonly IReadOnlyList<string> DreamSetPrototypes = new[]
         {
-            "adjectives",
-            "names_first",
-            "verbs",
+            "dreamadjectives", 
+            "dreamadverbs", 
+            "dreamingverbs", 
+            "dreamstrings", 
+            "dreamverbs", 
+            "nightmare"
         };
 
         public override void Update(float frameTime)
         {
-            base.Update(frameTime);
             _accumulator += frameTime;
             if (_accumulator < _updateRate)
                 return;
 
             _accumulator -= _updateRate;
-            _updateRate = _random.NextFloat(10f, 30f);
+            _updateRate = _random.NextFloat(30f, 60f);
 
             foreach (var sleeper in EntityQuery<SleepingComponent>())
             {
-                if (!TryComp<ActorComponent>(sleeper.Owner, out var actor))
+                if (!TryComp<ActorComponent>(sleeper.Owner, out var actor) || !TryComp<MoodComponent>(sleeper.Owner, out var mood))
                     continue;
 
-                var setName = _random.Pick(DreamSetPrototypes);
+                var dreamFragments = GenerateDreamOrNightmare(mood);
+                ShowDreamFragmentsAsync(dreamFragments, sleeper.Owner, actor, mood);
+            }
+        }
 
-                if (!_prototypeManager.TryIndex<DatasetPrototype>(setName, out var set))
-                    return;
+        private List<string> GenerateDreamOrNightmare(MoodComponent mood)
+        {
+            var dreamFragments = new List<string>();
 
-                var msg = _random.Pick(set.Values) + "..."; //todo... does the seperator need loc?
+            if (mood.CurrentMoodLevel <= 40 && _random.Prob(50))
+            {
+                dreamFragments.Add(Loc.GetString("chat-manager-dream-see"));
 
-                var messageWrap = Loc.GetString("chat-manager-send-telepathic-chat-wrap-message",
-                    ("telepathicChannelName", Loc.GetString("chat-manager-telepathic-channel-name")), ("message", msg));
+                dreamFragments.Add(ProcessAdjective(GetRandomFragment("nightmare")));
+                dreamFragments.Add(_random.Prob(0.5f) 
+                    ? (_random.Prob(0.35f) ? GetRandomFragment("dreamadverbs") + " " : "") + GetRandomFragment("dreamingverbs")
+                    : Loc.GetString("chat-manager-dream-willbe") + " " + GetRandomFragment("dreamverbs"));
 
-                _chatManager.ChatMessageToOne(Shared.Chat.ChatChannel.Telepathic,
-                msg, messageWrap, sleeper.Owner, false, actor.PlayerSession.ConnectedClient, Color.PaleVioletRed);
+                dreamFragments.Add(ProcessAdjective(GetRandomFragment("nightmare")));
+            }
+            else
+            {
+                dreamFragments.Add(Loc.GetString("chat-manager-dream-see"));
+                dreamFragments.Add(ProcessAdjective(GetRandomFragment("dreamstrings")));
+                dreamFragments.Add(_random.Prob(0.5f)
+                    ? (_random.Prob(0.35f) ? GetRandomFragment("dreamadverbs") + " " : "") + GetRandomFragment("dreamingverbs")
+                    : Loc.GetString("chat-manager-dream-willbe") + " " + GetRandomFragment("dreamverbs"));
+
+                dreamFragments.Add(ProcessAdjective(GetRandomFragment("dreamstrings")));
+            }
+
+            return dreamFragments;
+        }
+
+        private string GetRandomFragment(string setName) =>
+            _prototypeManager.TryIndex<DatasetPrototype>(setName, out var set) ? _random.Pick(set.Values) : "";
+
+        private string ProcessAdjective(string fragment)
+        {
+            if (_random.Prob(0.5f))
+            {
+                var adjective = GetRandomFragment("dreamadjectives");
+                fragment = fragment.Replace("%ADJECTIVE%", adjective);
+            }
+            else
+            {
+                fragment = fragment.Replace("%ADJECTIVE% ", "");
+            }
+            return fragment.Replace("%A% ", "\a ");
+        }
+
+        private async void ShowDreamFragmentsAsync(List<string> fragments, EntityUid sleeper, ActorComponent actor, MoodComponent mood)
+        {
+            foreach (var fragment in fragments)
+            {
+                if (!EntityManager.HasComponent<SleepingComponent>(sleeper))
+                    break;
+
+                var messageColor = mood.CurrentMoodLevel <= 40 && _random.Prob(50) ? Color.DarkRed : Color.BlueViolet;
+                _chatManager.ChatMessageToOne(Shared.Chat.ChatChannel.Telepathic, fragment, Loc.GetString("chat-manager-send-dream-chat-wrap-message", ("message", fragment)), sleeper, false, actor.PlayerSession.ConnectedClient, messageColor);
+
+                await Task.Delay((int)_random.NextFloat(5000f, 10000f));
             }
         }
     }
