fa34c96bd0b4d3dd5fda376a155a90a5a42c4b84
Evgencheg <7064926@gmail.com>
2024-10-19 12:14:36 +0300

    фикус

--- Tools/ss14_ru/translation.bat
+++ Tools/ss14_ru/translation.bat
@@ -1,9 +1,7 @@
-﻿@echo off
-
-call pip install -r requirements.txt --no-warn-script-location
-call python ./yamlextractor.py
-call python ./keyfinder.py
-call python ./clean_ftl.py
-call python ./1.py
+﻿pip install -r requirements.txt --no-warn-script-location
+python ./yamlextractor.py
+python ./keyfinder.py
+python ./clean_ftl.py
+python ./1.py
 
 PAUSE
